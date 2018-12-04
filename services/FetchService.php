ed61592f91fb7877cb943d9b2b46565dc280982c
sascha.bast <Qicklink1>
2018-12-04 13:42:03 +0100

    refactoring

--- services/FetchService.php
+++ services/FetchService.php
@@ -46,7 +46,7 @@ class  FetchService
 
     public function download($url, $loc)
     {
-        $file_path = fopen($loc, 'wab+');
+        $file_path = fopen($loc, 'w+');
         $client = new \GuzzleHttp\Client();
         $response = $client->get($url, ['save_to' => $file_path]);
         return ['response_code' => $response->getStatusCode(), 'name' => $loc];
